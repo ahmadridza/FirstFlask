b605a6d8655c0bb528e87b471f548c937558e29d
ahmadridza <59468419+ahmadridza@users.noreply.github.com>
2020-02-24 17:54:23 +0800

    inheritence in template

--- Flask/server.py
+++ Flask/server.py
@@ -4,18 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    signed_in = False
-    return render_template('index.html', signed_in=signed_in)
+    return render_template('index.html')
 
 
-@app.route("/another")
-def show1():
-    return '<h1> Yo this is your another page</h1>'
-
-
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username[5]}"
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
