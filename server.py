40c7b1847c0e2b82c9d7bcfd10d633d2ab8900e9
ahmadridza <59468419+ahmadridza@users.noreply.github.com>
2020-02-24 12:23:22 +0800

    add show route

--- server.py
+++ server.py
@@ -7,5 +7,10 @@ def index():
     return '<h1>Why liddat?</h1>'
 
 
+@app.route("/another")
+def show():
+    return '<h1> Yo this is your another page</h1>'
+
+
 if __name__ == '__main__':
     app.run()
